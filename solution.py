a94170956d65344dc9279f289edf006fbb4df376
mmartiss <martynasabrai@gmail.com>
2025-02-05 19:39:57 +0200

    The given start of the file and fuel consumtion comment.

---
+++ solution.py
@@ -0,0 +1,27 @@
+# fuel consumption = base fuel * distance * (1+total weight/max capacity)
+
+
+def find_optimal_route_for_single_van(van_stats: list[tuple[int, int]], packages: list[tuple[int, int, int]]) -> tuple[
+    tuple[int, int], list[tuple[int, str]], int, int]:
+    # TODO: Replace this with a real implementation:
+    return (
+        (9, 8),
+        [(0, 'start'), (-1, 'pick'), (-2, 'pick'), (5, 'drop'), (9, 'drop'), (6, 'pick'), (2, 'drop'), (0, 'end')],
+        22,
+        176
+    )
+
+if __name__ == "__main__":
+    # This is an example test for Base goal. When evaluating the task, more will be added:
+    selected_van, optimal_route, route_length, fuel_consumption = find_optimal_route_for_single_van(
+        [(10, 10), (9, 8)],  [(-1, 5, 4), (6, 2, 9), (-2, 9, 3)]
+    )
+
+    assert selected_van == (9, 8)
+    assert optimal_route == [
+        (0, 'start'), (-1, 'pick'), (-2, 'pick'), (5, 'drop'), (9, 'drop'), (6, 'pick'), (2, 'drop'), (0, 'end')
+    ]
+    assert route_length == 22
+    assert fuel_consumption == 176
+
+    print("ALL TESTS PASSED")
